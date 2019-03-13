4429d4fc772f056008ace848ce71e3514aefb239
Barbara Fick <bkantzos@yahoo.com>
2019-03-13 16:06:49 -0600

    routerLinks buttons to Search and Discover pages, styling main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   //list routes from the most specific routes to least specific route
-  { path: '', redirectTo: '/about', pathMatch: 'full' }, //pathMath: 'prefix' is the other possible value meaning match anything that starts with
+  { path: '', component: AboutComponent, pathMatch: 'full' }, //pathMath: 'prefix' is the other possible value meaning match anything that starts with
   { path: 'about', component: AboutComponent },
   { path: 'discover', component: DiscoverComponent },
   { path: 'search', component: SearchComponent },

b616cc1d036cad1e977c156d37176edb0279d73f
Barbara Fick <bkantzos@yahoo.com>
2019-03-14 21:18:57 -0600

    trying public puppies:any = {};

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -8,7 +8,7 @@ import { PuppyService } from '../puppy.service';
 })
 export class DiscoverComponent implements OnInit {
 
-  public puppies: Array<any>;
+  public puppies:any = {};
   public pupFriendCount = 0;
   public friendMatch: boolean = false;
   public matchMessage: String = '';
